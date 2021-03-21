4907c3e53e53df1f11f8220e5e1534503557eee9
fdz23 <ferdavid_9@hotmail.com>
2021-03-21 17:29:47 -0300

    Cria rota para segunda parte

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,4 +1,5 @@
 class AppRoutes {
   static const LOGIN = '/login';
-  static const CADASTRO_PACIENTE = '/cadastro-paciente';
+  static const CADASTRO_PACIENTE_INICIO = '/cadastro-paciente-inicio';
+  static const CADASTRO_PACIENTE_FIM = '/cadastro-paciente-fim';
 }

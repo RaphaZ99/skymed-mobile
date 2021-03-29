c406a5283cabfdc02e89b8478e2dd241a3dc8d94
raphael <raphael.mota14@hotmail.com>
2021-03-28 21:33:14 -0300

    Criação - Adicionado rotas de novas telas de redefinição de senha

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -2,4 +2,9 @@ class AppRoutes {
   static const LOGIN = '/login';
   static const CADASTRO_PACIENTE_INICIO = '/cadastro-paciente-inicio';
   static const CADASTRO_PACIENTE_FIM = '/cadastro-paciente-fim';
+  static const RECUPERAR_SENHA_PACIENTE_EMAIL = '/recuperar-senha-paciente-email';
+  static const RECUPERAR_SENHA_PACIENTE_CODIGO = '/recuperar-senha-paciente-codigo';
+  static const RECUPERAR_SENHA_PACIENTE_NOVA_SENHA = '/recuperar-senha-paciente-nova-senha';
+
+
 }

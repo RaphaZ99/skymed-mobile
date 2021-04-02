96f24ad51c1fb3b3da1f75a2e944daf97c1ee05c
fdz23 <ferdavid_9@hotmail.com>
2021-04-01 22:05:30 -0300

    Cria view do usuario

---
+++ lib/views/menu-usuario.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:skymed_mobile/views/edicao-paciente-dados.dart';
+import 'package:skymed_mobile/views/edicao-paciente-endereco.dart';
+import 'package:skymed_mobile/views/login.dart';
+import 'package:skymed_mobile/widgets/componentes/app-bar/logo.dart';
+import 'package:skymed_mobile/widgets/componentes/card-campo/botao.dart';
+import 'package:skymed_mobile/widgets/componentes/card-campo/campo.dart';
+import 'package:skymed_mobile/widgets/componentes/card-campo/icone-campo.dart';
+import 'package:skymed_mobile/widgets/componentes/tema/circulos-topo.dart';
+
+import 'cadastro-paciente-fim.dart';
+
+class WidgetMenuUsuario extends StatefulWidget {
+  @override
+  _WidgetMenuUsuario createState() => _WidgetMenuUsuario();
+}
+
+class _WidgetMenuUsuario extends State<WidgetMenuUsuario> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        padding: const EdgeInsets.only(
+          left: 30.0,
+          right: 30.0,
+          top: 60.0,
+        ),
+        child: ListView(
+          padding: const EdgeInsets.only(
+            top: 50,
+          ),
+          children: <Widget>[
+            IconeCampo(
+              titulo: 'Nome Usuário',
+              icone: Icon(
+                Icons.person_pin,
+                size: 70.0,
+              ),
+              tamanhoFonte: 30.0,
+            ),
+            IconeCampo(
+              titulo: 'Histórico de Consultas',
+              icone: Icon(
+                Icons.history,
+                size: 35.0,
+              ),
+              tamanhoFonte: 20.0,
+              margem: EdgeInsets.only(
+                left: 15.0,
+                top: 30.0,
+              ),
+              callback: () {},
+            ),
+            IconeCampo(
+              titulo: 'Editar Cadastro',
+              icone: Icon(
+                Icons.edit,
+                size: 35.0,
+              ),
+              tamanhoFonte: 20.0,
+              margem: EdgeInsets.only(
+                left: 15.0,
+                top: 30.0,
+              ),
+              callback: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => WidgetEdicaoPacienteDados()),
+                );
+              },
+            ),
+            IconeCampo(
+              titulo: 'Editar Endereço',
+              icone: Icon(
+                Icons.edit_outlined,
+                size: 35.0,
+              ),
+              tamanhoFonte: 20.0,
+              margem: EdgeInsets.only(
+                left: 15.0,
+                top: 30.0,
+              ),
+              callback: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => WidgetEdicaoPacienteEndereco()),
+                );
+              },
+            ),
+            IconeCampo(
+              titulo: 'Sair',
+              icone: Icon(
+                Icons.history,
+                size: 35.0,
+              ),
+              tamanhoFonte: 20.0,
+              margem: EdgeInsets.only(
+                left: 15.0,
+                top: 30.0,
+              ),
+              callback: () {
+                Navigator.popUntil(context, ModalRoute.withName('/login'));
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

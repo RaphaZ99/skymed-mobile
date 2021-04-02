33dfa6ced20f2df60102e82b024c83f0b0775239
fdz23 <ferdavid_9@hotmail.com>
2021-04-02 12:43:39 -0300

    Utiliza botão padrão

--- lib/views/cadastro-paciente-inicio.dart
+++ lib/views/cadastro-paciente-inicio.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:skymed_mobile/widgets/componentes/app-bar/logo.dart';
 import 'package:skymed_mobile/widgets/componentes/card-campo/botao.dart';
 import 'package:skymed_mobile/widgets/componentes/card-campo/campo.dart';
+import 'package:skymed_mobile/widgets/componentes/padroes/voltar-padrao.dart';
 import 'package:skymed_mobile/widgets/componentes/tema/circulos-topo.dart';
 
 import 'cadastro-paciente-fim.dart';
@@ -78,16 +79,7 @@ class _WidgetCadastroPacienteInicioState
                     )
                   },
                 ),
-                Botao(
-                  titulo: 'Voltar',
-                  margem: EdgeInsets.only(
-                    top: 15,
-                  ),
-                  corBorda: Colors.red,
-                  corInterna: Colors.white,
-                  corLetra: Colors.black,
-                  callback: () => {Navigator.pop(context)},
-                ),
+                BotaoVoltarPadrao(),
               ],
             ),
           ),
--- lib/views/edicao-paciente-endereco.dart
+++ lib/views/edicao-paciente-endereco.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:skymed_mobile/widgets/componentes/app-bar/logo.dart';
 import 'package:skymed_mobile/widgets/componentes/card-campo/botao.dart';
 import 'package:skymed_mobile/widgets/componentes/card-campo/campo.dart';
+import 'package:skymed_mobile/widgets/componentes/padroes/voltar-padrao.dart';
 import 'package:skymed_mobile/widgets/componentes/tema/circulos-topo.dart';
 
 class WidgetEdicaoPacienteEndereco extends StatefulWidget {
@@ -70,16 +71,7 @@ class _WidgetEdicaoPacienteEnderecoState
                   ),
                   callback: () => {Navigator.pop(context)},
                 ),
-                Botao(
-                  titulo: 'Voltar',
-                  margem: EdgeInsets.only(
-                    top: 15,
-                  ),
-                  corBorda: Colors.red,
-                  corInterna: Colors.white,
-                  corLetra: Colors.black,
-                  callback: () => {Navigator.pop(context)},
-                ),
+                BotaoVoltarPadrao(),
               ],
             ),
           ),

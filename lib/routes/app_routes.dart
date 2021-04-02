6b448b0283ef78d19b0fc4d27d5d42e7d954e6e4
fdz23 <ferdavid_9@hotmail.com>
2021-04-01 21:22:08 -0300

    Cria rota das views de edição de cadastro

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -2,5 +2,7 @@ class AppRoutes {
   static const LOGIN = '/login';
   static const CADASTRO_PACIENTE_INICIO = '/cadastro-paciente-inicio';
   static const CADASTRO_PACIENTE_FIM = '/cadastro-paciente-fim';
+  static const EDICAO_PACIENTE_DADOS = '/edicao-paciente-dados';
+  static const EDICAO_PACIENTE_ENDERECO = '/edicao-paciente-endereco';
   static const LISTAGEM_MEDICOS = '/listagem-medicos';
 }

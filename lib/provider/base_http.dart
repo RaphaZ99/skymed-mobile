c22253b3371265983bf4a960e211b7c8fec2e765
fdz23 <ferdavid_9@hotmail.com>
2021-06-16 19:38:48 -0300

    Cria método base de http

---
+++ lib/provider/base_http.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
+
+class BaseHttp with ChangeNotifier {
+  final Map<String, String> headerPadrao = {
+    'Content-type': 'application/json',
+    'Accept': 'application/json',
+  };
+  final Map<String, String> headerAutenticado = {
+    'Content-type': 'application/json',
+    'Accept': 'application/json',
+    'Token': tokenJWT,
+  };
+  static String tokenJWT = "";
+  static bool usuarioLogado = tokenJWT != "";
+
+  Future<http.Response> postPadrao(Object body, Uri url) async {
+    return await post(body, headerPadrao, url);
+  }
+
+  Future<http.Response> postAutenticado(Object body, Uri url) async {
+    return await post(body, headerAutenticado, url);
+  }
+
+  Future<http.Response> post(
+      Object body, Map<String, String> headers, Uri url) async {
+    await http
+        .post(
+      url,
+      body: body,
+      headers: headers,
+    )
+        .then((value) {
+      return value;
+    });
+
+    notifyListeners();
+    return Future.value();
+  }
+}

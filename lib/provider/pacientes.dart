5ed87fa8aab31ac58c2ed91993da796f4cd4aade
fdz23 <ferdavid_9@hotmail.com>
2021-06-16 19:39:02 -0300

    Ajusta pacientes com base no http base

--- lib/provider/pacientes.dart
+++ lib/provider/pacientes.dart
@@ -1,41 +1,26 @@
 import 'dart:convert';
-
 import 'package:flutter/cupertino.dart';
-import 'package:http/http.dart' as http;
 import 'package:skymed_mobile/model/paciente.dart';
+import 'package:skymed_mobile/provider/base_http.dart';
 
 class Pacientes with ChangeNotifier {
   final _baseUrl = 'https://skymed-api.herokuapp.com';
+  final _baseHttp = new BaseHttp();
 
-  String _token;
+  Future<void> authenticate(String senha, String email, context) async {
+    final Object body = json.encode(
+      {
+        "email": email,
+        "senha": senha,
+      },
+    );
 
-  bool get isAutenticado {
-    return token != null;
-  }
-
-  String get token {
-    if (_token != null) {
-      return _token;
-    }
-  }
-
-  Future<void> authenticate(String senha, String email) async {
-    await http.post(Uri.parse('$_baseUrl/login'),
-        body: json.encode(
-          {
-            "email": email,
-            "senha": senha,
-          },
-        ),
-        headers: {
-          'Content-type': 'application/json',
-          'Accept': 'application/json',
-        }).then((value) {
-      print(value.statusCode);
-      print(value.request);
-      print(value.body);
+    await _baseHttp
+        .postPadrao(body, Uri.parse('$_baseUrl/login'))
+        .then((value) {
       final responseBody = json.decode(value.body);
-      _token = responseBody['token'];
+
+      BaseHttp.tokenJWT = responseBody['token'];
     });
     notifyListeners();
 
@@ -43,38 +28,34 @@ class Pacientes with ChangeNotifier {
   }
 
   Future<void> adicionarPaciente(Paciente paciente) async {
-    await http.post(Uri.parse('$_baseUrl/pessoa'),
-        body: json.encode({
-          "nome": paciente.nome,
-          "cpf": paciente.cpf,
-          "rg": paciente.rg,
-          "email": paciente.email,
-          "telefone": paciente.telefone,
-          "origemPaciente": paciente.origemPaciente,
-          "endereco": {
-            "cep": paciente.endereco.cep,
-            "bairro": paciente.endereco.bairro,
-            "logradouro": paciente.endereco.logradouro,
-            "complemento": paciente.endereco.complemento,
-            "numero": paciente.endereco.numero
-          },
-          "usuario": {
-            "ehAdmin": paciente.usuario.ehAdmin,
-            "ehMedidco": paciente.usuario.ehMedico,
-            "ehAutenticado": paciente.usuario.ehAutenticado,
-            "ehHospital": paciente.usuario.ehHospital,
-            "ehPaciente": paciente.usuario.ehPaciente,
-            "email": paciente.usuario.email,
-            "senha": paciente.usuario.senha,
-          }
-        }),
-        headers: {
-          'Content-type': 'application/json',
-          'Accept': 'application/json',
-        }).then((value) {
-      print(value.statusCode);
-      print(value.request);
-      print(value.body);
+    final Object body = json.encode({
+      "nome": paciente.nome,
+      "cpf": paciente.cpf,
+      "rg": paciente.rg,
+      "email": paciente.email,
+      "telefone": paciente.telefone,
+      "origemPaciente": paciente.origemPaciente,
+      "endereco": {
+        "cep": paciente.endereco.cep,
+        "bairro": paciente.endereco.bairro,
+        "logradouro": paciente.endereco.logradouro,
+        "complemento": paciente.endereco.complemento,
+        "numero": paciente.endereco.numero
+      },
+      "usuario": {
+        "ehAdmin": paciente.usuario.ehAdmin,
+        "ehMedidco": paciente.usuario.ehMedico,
+        "ehAutenticado": paciente.usuario.ehAutenticado,
+        "ehHospital": paciente.usuario.ehHospital,
+        "ehPaciente": paciente.usuario.ehPaciente,
+        "email": paciente.usuario.email,
+        "senha": paciente.usuario.senha,
+      }
+    });
+
+    await _baseHttp
+        .postPadrao(body, Uri.parse('$_baseUrl/pessoa'))
+        .then((value) {
       return value.statusCode;
     });
   }

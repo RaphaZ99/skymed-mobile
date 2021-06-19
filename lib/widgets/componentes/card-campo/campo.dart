0af872cff1f2a0341c32df1e8fd1c6c49b547cfb
fdz23 <ferdavid_9@hotmail.com>
2021-06-19 16:44:22 -0300

    Adiciona controller para obtencao do campo

--- lib/widgets/componentes/card-campo/campo.dart
+++ lib/widgets/componentes/card-campo/campo.dart
@@ -2,17 +2,19 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Campo extends StatelessWidget {
-  Campo({this.titulo, this.ehSenha, this.margem});
+  Campo({this.titulo, this.ehSenha, this.margem, this.controller});
 
   final String titulo;
   final bool ehSenha;
   final EdgeInsets margem;
+  final TextEditingController controller;
 
   @override
   Widget build(BuildContext context) {
     return Card(
       child: ListTile(
         title: TextField(
+          controller: this.controller,
           textInputAction: TextInputAction.next,
           obscureText: ehSenha ?? false,
           decoration: InputDecoration(

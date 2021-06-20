2171016b62547da964b0362ad8c8f1ff2598708e
fdz23 <ferdavid_9@hotmail.com>
2021-06-19 22:24:29 -0300

    Ajusta modal de consulta

--- lib/widgets/componentes/modal/modal-consulta.dart
+++ lib/widgets/componentes/modal/modal-consulta.dart
@@ -3,6 +3,8 @@ import 'package:skymed_mobile/widgets/componentes/card-campo/botao.dart';
 import 'package:skymed_mobile/widgets/componentes/card-campo/campo.dart';
 
 class ModalConsulta extends StatelessWidget {
+  final TextEditingController controller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
@@ -26,11 +28,14 @@ class ModalConsulta extends StatelessWidget {
           height: 150.0,
           child: ListView(
             children: [
-              Campo(titulo: 'Horário'),
+              Campo(
+                titulo: 'Horário',
+                controller: controller,
+              ),
               Botao(
                 titulo: 'Confirmar',
                 callback: () {
-                  Navigator.pop(context);
+                  Navigator.pop(context, controller.text);
                 },
               )
             ],
